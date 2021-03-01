959fefda6ae71da1dc36489cb3c7ea364d841399
Shashank Pradhan <shashankpradhan911@gmail.com>
2021-03-01 23:29:42 +0530

    Day 22 started

---
+++ Day 22/22.py
@@ -0,0 +1,10 @@
+## Day 22 of 100 days of code
+
+#Circular Linked List Continues
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class CircularLinked List:
