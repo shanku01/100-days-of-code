5f212304fe82dfff58a7a485ebbb1153939e41db
Shashank Pradhan <shashankpradhan911@gmail.com>
2021-03-15 02:37:10 +0530

    Day 35

--- Day 35/35.py
+++ Day 35/35.py
@@ -22,11 +22,36 @@ def push(head,ndata):
     head = node
     return head
    
+def deleteNode(head,key):
+
+    #base case
+    if head is None or key is None:
+        print("List or Data not found")
+        return
+
+    temp = head
+
+    while(temp.data !=key):
+        temp=temp.next
+        if temp is head:
+            print("Data not found")
+            return
+        
+    if temp.next !=temp:
+         temp.prev.next = temp.next
+         temp.next.prev= temp.prev
+    else:
+        head = None
+
+    del temp
+
+    return head
 
 def printList(head):
     temp = head
     
     if temp is None:
+        print("List is empty")
         return
     
     while(temp.next!=head):
@@ -41,10 +66,17 @@ head = None
 head = push(head,2)
 head = push(head,3)
 head = push(head,4)
-head = push(head,5)
 head = push(head,6)
+head = push(head,7)
 printList(head)
 
 
+head = deleteNode(head,2)
+head = deleteNode(head,4)
+head = deleteNode(head,6)
 printList(head)
+
+head = deleteNode(head,3)
+printList(head)
+
 
