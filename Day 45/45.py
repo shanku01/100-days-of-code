e420cb03deb27901c211c34eff5d854349d63009
Shashank Pradhan <shashankpradhan911@gmail.com>
2021-03-25 14:45:18 +0530

    Day 45 start

--- Day 45/45.py
+++ Day 45/45.py
@@ -1,25 +1,9 @@
-# Day 45 of 100 daysof code
+# Day 45 of 100 Days of Code
+
+# Binary Tree
 
 class Node:
-    def __init__(self,data = None,left = None, right = None):
+    def __init__(self,data=None,left=None,right=None):
         self.data = data
-        self.left = left
+        self.left = left 
         self.right = right
-        
-
-
-# Testing
-leaf1 = Node(0)
-leaf2 = Node(1)
-leaf3 = Node(2)
-leaf4 = Node(3)
-
-node1 = Node(4,leaf1,leaf2)
-node2 = Node(5,leaf3,leaf4)
-
-root = Node(6,node1,node2)
-
-
-
-    
-    
