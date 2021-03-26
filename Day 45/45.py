b35c570f09a94d2382a397df3f285ad009e4f0ff
Shashank Pradhan <shashankpradhan911@gmail.com>
2021-03-27 01:32:50 +0530

    Day 45 intiate

--- Day 45/45.py
+++ Day 45/45.py
@@ -1,1 +1,33 @@
 # Day 45 of 100 Days of Code
+
+# Getting postorder traversal from pre and inorder
+
+def search(arr, x, n):
+
+    for i in range(n):
+        if (arr[i]==x):
+            return i
+
+    return -1
+
+def getPostorder(inorder, preorder, n):
+
+    root = search(inorder,preorder[0],n)
+
+    if (root!= None):
+        getPostorder(inorder,preorder[1:n],root)
+
+    if(root != n-1):
+        getPostorder(inoder[root+1:n],preoder[root+1:n],n-root-1)
+
+    print(pre[0],end='')
+
+# Testing
+
+In = [ 4, 2, 5, 1, 3, 6 ]
+pre = [ 1, 2, 4, 5, 3, 6 ]
+n = len(In)
+ 
+print("Postorder Traversal")
+ 
+printPostOrder(In, pre, n)
